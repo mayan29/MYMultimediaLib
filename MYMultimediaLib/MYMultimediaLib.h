9d8d472608901a686ec7f63bc640169bc2aaf8cf
mayan <may-g@glodon.com>
2018-12-07 12:26:44 +0800

    Initialization

---
+++ MYMultimediaLib/MYMultimediaLib.h
@@ -0,0 +1,19 @@
+//
+//  MYMultimediaLib.h
+//  MYMultimediaLib
+//
+//  Created by mayan on 2018/12/7.
+//  Copyright © 2018 mayan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MYMultimediaLib.
+FOUNDATION_EXPORT double MYMultimediaLibVersionNumber;
+
+//! Project version string for MYMultimediaLib.
+FOUNDATION_EXPORT const unsigned char MYMultimediaLibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MYMultimediaLib/PublicHeader.h>
+
+
---
+++ MYMultimediaLibDemo/MYMultimediaLibDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MYMultimediaLibDemo
+//
+//  Created by mayan on 2018/12/7.
+//  Copyright © 2018 mayan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
